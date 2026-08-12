554ae0b8ff5c08edaf49bb6750886e4f5ede73a5
JustinYSJSU <justinkaiy02@gmail.com>
2026-08-12 12:43:29 -0700

    first version of retry logic

--- run_command.py
+++ run_command.py
@@ -3,7 +3,8 @@
 import subprocess
 import shlex
 import argparse
-
+import sys
+import time
 parser = argparse.ArgumentParser(prog='run_command', description='run/re-run terminal commands', epilog='see help')
 parser.add_argument('--retries', required=True)
 parser.add_argument('--delay', required=True)
@@ -11,9 +12,24 @@ parser.add_argument('--command', required=True)
 
 def pass_command(retries, delay, command):
     split_command = shlex.split(command)
+    retries = int(retries)
 
-    subprocess.run([split_command[0]])
+    run_attempt = subprocess.run(split_command, capture_output=True, text=True)
+    if run_attempt.returncode != 0 and retries > 0:
+        attempts = 0
+        while retries > 0 and run_attempt.returncode != 0:
+            retries -= 1
+            attempts += 1
+            print(f"Attempt failed with exit code {run_attempt.returncode}")
+            print(f"Retrying in {delay}s...")
+            time.sleep(int(delay))
+            run_attempt = subprocess.run(split_command, capture_output=True, text=True)
+        if run_attempt.returncode != 0:
+            print(f"Comamnd failed after {attempts} attempts")
 
+    # except FileNotFoundError:
+        # print("Command not found.")
+        # sys.exit(1)
 def main():
     args = parser.parse_args()
     retries = args.retries
