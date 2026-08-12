9b26628747fdcc3f88ce433877c03a1713a783bb
JustinYSJSU <justinkaiy02@gmail.com>
2026-08-12 01:32:38 -0700

    first addtions for comamnd run (using linux)

--- run_command.py
+++ run_command.py
@@ -1,4 +1,7 @@
+# RUN IN WSL FOR LINUX
+
 import subprocess
+import shlex
 import argparse
 
 parser = argparse.ArgumentParser(prog='run_command', description='run/re-run terminal commands', epilog='see help')
@@ -6,7 +9,17 @@ parser.add_argument('--retries', required=True)
 parser.add_argument('--delay', required=True)
 parser.add_argument('--command', required=True)
 
-args = parser.parse_args()
-print(args.retries)
-print(args.delay)
-print(args.command)
+def pass_command(retries, delay, command):
+    split_command = shlex.split(command)
+
+    subprocess.run([split_command[0]])
+
+def main():
+    args = parser.parse_args()
+    retries = args.retries
+    delay = args.delay
+    command = args.command
+    pass_command(retries=retries, delay=delay, command=command)
+
+if __name__ == "__main__":
+    main()
