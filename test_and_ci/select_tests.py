1a828b82c7468c99383e1d51e2d7e21bf481d728
JustinYSJSU <justinkaiy02@gmail.com>
2026-08-15 12:20:55 -0700

    add argument for --include-disabled

--- test_and_ci/select_tests.py
+++ test_and_ci/select_tests.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(prog='select tests', description='select test f
 parser.add_argument("file_name")
 parser.add_argument("--component", required=False)
 parser.add_argument("--priority", required=False)
-
+parser.add_argument("--include-disabled", action='store_true') # if provided => true | if not provided => false
 args = parser.parse_args()
 
 def validate_json_file(file_name):
@@ -25,12 +25,17 @@ def load_json_to_dict(file_name):
         data = json.load(f)
     return data
 
+def filter_tests(dict, test_component, test_priority, include_disabled):
+    print(0)
+
 def main():
     file_name = args.file_name
     test_component = args.component
     test_priority = args.priority
-
+    include_disabled = args.include_disabled
     validate_json_file(file_name)
-    valid_dict = load_json_to_dict(file_name, test_component=None, test_priority=None)
+    valid_dict = load_json_to_dict(file_name=file_name)
+    filter_tests(dict=valid_dict, test_component=test_component, test_priority=test_priority, include_disabled=include_disabled)
+
 if __name__ == "__main__":
     main()
