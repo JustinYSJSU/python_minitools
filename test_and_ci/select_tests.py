26af5f746570fc9a9878b4d49fb72fb8c94acfb1
JustinYSJSU <justinkaiy02@gmail.com>
2026-08-16 17:29:22 -0700

    finish select_tests

--- test_and_ci/select_tests.py
+++ test_and_ci/select_tests.py
@@ -11,7 +11,6 @@ parser.add_argument("--include-disabled", action='store_true') # if provided => 
 args = parser.parse_args()
 
 def validate_json_file(file_name):
-
     path_name = Path(file_name)
     if not path_name.is_file():
         print(f"{path_name} not found")
@@ -25,17 +24,45 @@ def load_json_to_dict(file_name):
         data = json.load(f)
     return data
 
-def filter_tests(dict, test_component, test_priority, include_disabled):
-    print(0)
+def create_filter_dict(arg_dict):
+    filter_dict = {}
+    for key, value in arg_dict.items():
+        if arg_dict[key] is not None:
+            filter_dict[key] = value
+    return filter_dict
+
+def filter_tests(test_dict, filter_dict):
+    final_tests = []
+    filter_dict_keys_no_disable = [key for key in filter_dict if key != "include_disabled"]
+
+    for test in test_dict['tests']:
+        matches = True
+        for key in filter_dict_keys_no_disable:
+            if test[key] != filter_dict[key]:
+                matches = False
+                break
+        if matches:
+            if not test["enabled"] and not filter_dict["include_disabled"]:
+                continue
+            else:
+                final_tests.append(test)
+    return final_tests
 
 def main():
     file_name = args.file_name
-    test_component = args.component
-    test_priority = args.priority
-    include_disabled = args.include_disabled
+    arg_dict = {
+        "component": args.component,
+        "priority": args.priority,
+        "include_disabled": args.include_disabled
+    }
+    filter_dict = create_filter_dict(arg_dict=arg_dict)
     validate_json_file(file_name)
-    valid_dict = load_json_to_dict(file_name=file_name)
-    filter_tests(dict=valid_dict, test_component=test_component, test_priority=test_priority, include_disabled=include_disabled)
+    test_dict = load_json_to_dict(file_name=file_name)
+    final_tests = filter_tests(test_dict=test_dict, filter_dict=filter_dict)
+
+    print(f"Selected {len(final_tests)} tests")
+    for test in final_tests:
+        print(test['name'])
 
 if __name__ == "__main__":
     main()
